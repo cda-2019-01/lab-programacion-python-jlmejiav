d58fa1d96c113e01221bf5d5908e367da51b61bd
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-17 00:17:41 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,24 @@
 ## D,23
 ## E,67
 ##
+data2=open('data.csv','r').readlines()
+data3 = [row[0:-1] for row in data2]
+data4 = [row.split('\t') for row in data3]
+col1 = [row[0] for row in data4]
+letras = sorted(set(col1))
+##
+sum=0
+vec=[]
+for w in letras:
+    sum=0
+    for row in data4:
+        if row[0] == w:
+            sum = sum + int(row[1])        
+    print(w,',',sum)
+##
+for w in letras:
+     ...:         sum=0
+     ...:         for row in data4:
+     ...:             if row[0] == w:
+     ...:                 sum = sum + int(row[1])
+     ...:         print(w,sum)
