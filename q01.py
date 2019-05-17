c2b45fe9147eb37ad0920b0c4d7a16993bf385b0
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-16 23:43:47 -0500

    parte 1 y 2 readys (el 2 no queda en lista como columna sino en fila)

--- q01.py
+++ q01.py
@@ -1,3 +1,10 @@
 ##
 ## Imprima la suma de la segunda columna.
-##
+##Por acá se convierte en líneas y texto
+data2=open('data.csv','r').readlines()
+data3 = [row[0:-1] for row in data2]
+data4 = [row.split('\t') for row in data3]
+sum=0
+for row in data4:
+    sum = sum + int(row[1])
+print(sum)
--- q02.py
+++ q02.py
@@ -8,3 +8,10 @@
 ## D,6
 ## E,14
 ##
+data2=open('data.csv','r').readlines()
+data3 = [row[0:-1] for row in data2]
+data4 = [row.split('\t') for row in data3]
+col1 = [row[0] for row in data4]
+letras = sorted(set(col1))
+conteo = ([(w, col1.count(w)) for w in letras])
+print(conteo)
